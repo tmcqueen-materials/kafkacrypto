1744a276dd79d6b8db9bf612f875807ad5fe6459
Tyrel M. McQueen <tmcqueen@github.com>
2019-10-30 21:47:17 -0400

    Prepare 0.9.9.1 bugfix release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="kafkacrypto",
-  version="0.9.9",
+  version="0.9.9.1",
   license="GNU GPLv2",
   keywords="kafka kafka-crypto kafka-security security crypo",
   author="Tyrel M. McQueen",
@@ -15,7 +15,8 @@ setuptools.setup(
   long_description_content_type="text/markdown",
   url="https://github.com/tmcqueen-materials/kafkacrypto",
   packages=setuptools.find_packages(),
-  install_requires=['pysodium','msgpack','kafka-python'],
+  python_requires='>=3.3',
+  install_requires=['pysodium>=0.7.1','msgpack>=0.6.0','kafka-python>=1.4.4'],
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
