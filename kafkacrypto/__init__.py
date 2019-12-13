670b18559b196e3eeb992c1b5d4c0fe2b2c263c2
Tyrel M. McQueen <tmcqueen@github.com>
2019-12-13 08:58:58 -0500

    Add message about version of backend Kafka in use.

--- kafkacrypto/__init__.py
+++ kafkacrypto/__init__.py
@@ -2,12 +2,18 @@ name='kafkacrypto'
 # version and license information in setup.py
 __all__ = []
 
+import logging
 try:
   import confluent_kafka
   from kafkacrypto.confluent_kafka_wrapper import KafkaConsumer,KafkaProducer,TopicPartition,TopicPartitionOffset,OffsetAndMetadata
+  logging.warning("Using confluent_kafka: %s, librdkafka: %s", confluent_kafka.version(), confluent_kafka.libversion())
 except ImportError:
   # fallback to kafka-python
+  logging.warning("No confluent_kafka package found. Falling back to kafka-python. It is highly")
+  logging.warning("recommended that you install confluent_kafka and librdkafka for better performance,")
+  logging.warning("especially with large messages.")
   from kafkacrypto.kafka_python_wrapper import KafkaConsumer,KafkaProducer,TopicPartition,TopicPartitionOffset,OffsetAndMetadata
+del logging
 
 __all__.extend(['KafkaConsumer', 'KafkaProducer', 'TopicPartition', 'TopicPartitionOffset', 'OffsetAndMetadata'])
 
