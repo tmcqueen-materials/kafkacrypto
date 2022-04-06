5bc8f6012dc7f70dc9102e98b16e3d331bb02d24
Tyrel M. McQueen <tmcqueen@github.com>
2022-04-06 09:51:13 -0400

    Add single config roundtrip public test.

--- public-tests/README.md
+++ public-tests/README.md
@@ -15,3 +15,18 @@ on all platforms.
   1. If desired, set `log_level : 20` in config.
   1. Make sure broker allows autocreation of topics, or create the topics `openmsitest.0`, `openmsitest.subs`, `openmsitest.reqs`, `openmsitest.keys`
   1. Run: `./roundtrip-test1.py`
+
+### roundtrip-test2
+This test carries out three successive full round-trips of encrypted data with a live broker. It execises many
+components of KafakCrypto, including configuration file handling, open and close operations, etc. Relies on
+group timeout not being "too long", since confluent-kafka-python does not gracefully support close operations
+on all platforms. This variant uses a single config file for both ends (not recommended in production, but
+invaluable in unit tests).
+
+  1. Install kafkacrypto
+  1. Use tools/simple-provision.py to generate one config: node3 (producer and consumer).
+  1. Adjust node3.config to point to live broker.
+  1. If desired, set `log_level : 20` in config.
+  1. Make sure broker allows autocreation of topics, or create the topics `openmsitest.0`, `openmsitest.subs`, `openmsitest.reqs`, `openmsitest.keys`
+  1. Run: `./roundtrip-test2.py`
+
---
+++ public-tests/roundtrip-test2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+from threading import Thread
+from kafkacrypto import KafkaCryptoStore, KafkaCrypto, KafkaConsumer, KafkaProducer
+from time import sleep
+from pysodium import randombytes
+
+class ProdThread(object):
+  def __init__(self,uval,kcs,joinon=None):
+    self._uval = uval
+    self._joinon = joinon
+    self.kcs = kcs
+    self._my_thread = Thread(target=self._my_thread_proc)
+    self._my_thread.start()
+
+  def _my_thread_proc(self):
+    # Setup KafkaCrypto
+    self.kcc = KafkaConsumer(**self.kcs.get_kafka_config('consumer',extra='crypto'))
+    self.kcp = KafkaProducer(**self.kcs.get_kafka_config('producer',extra='crypto'))
+    self.kc = KafkaCrypto(None,self.kcp,self.kcc,config=self.kcs)
+    kafka_config = self.kcs.get_kafka_config('producer')
+    kafka_config['key_serializer'] = self.kc.getKeySerializer()
+    kafka_config['value_serializer'] = self.kc.getValueSerializer()
+    self.producer = KafkaProducer(**kafka_config)
+    sleep(5)
+    self.producer.send("openmsitest.0",value=self._uval)
+    self.producer.poll(1)
+    sleep(5)
+    self.producer.poll(1)
+    if not (self._joinon is None):
+      self._joinon.join()
+    self.producer.close()
+    self.kcc.close()
+    self.kcp.close()
+    self.kc.close()
+
+class ConsumeThread(object):
+  def __init__(self,uval):
+    self._uval = uval
+    self.kcs = KafkaCryptoStore("node3.config")
+    self._my_thread = Thread(target=self._my_thread_proc)
+    self._my_thread.start()
+
+  def _my_thread_proc(self):
+    # Setup KafkaCrypto
+    self.kcc = KafkaConsumer(**self.kcs.get_kafka_config('consumer',extra='crypto'))
+    self.kcp = KafkaProducer(**self.kcs.get_kafka_config('producer',extra='crypto'))
+    self.kc = KafkaCrypto(None,self.kcp,self.kcc,config=self.kcs)
+    kafka_config = self.kcs.get_kafka_config('consumer')
+    kafka_config['key_deserializer'] = self.kc.getKeyDeserializer()
+    kafka_config['value_deserializer'] = self.kc.getValueDeserializer()
+    self.consumer = KafkaConsumer(**kafka_config)
+    self.consumer.subscribe(["openmsitest.0"])
+    cnt = 0
+    rcvd = False
+    while not rcvd and cnt < 65:
+      rv = self.consumer.poll(timeout_ms=1000)
+      for tp,msgs in rv.items():
+        for msg in msgs:
+          if bytes(msg.value) == self._uval:
+            print("Message Successfully Received!")
+            rcvd = True
+    self.consumer.close()
+    self.kcc.close()
+    self.kcp.close()
+    self.kc.close()
+    if not rcvd:
+      raise RuntimeError("Value Not Received!")
+
+for i in range(0,3):
+  uval = randombytes(64)
+  t2 = ConsumeThread(uval)
+  t1 = ProdThread(uval,t2.kcs,t2._my_thread)
+  t1._my_thread.join(65)
+
