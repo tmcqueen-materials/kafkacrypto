f2eb0352a8ba41ceb2db32c7be83b0fc4992d247
Tyrel M. McQueen <tmcqueen@github.com>
2023-11-29 13:55:58 -0500

    Prepare 0.9.10.1 release, with one change to make installation on Python 3.12+ more seamless.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="kafkacrypto",
-  version="0.9.10.0",
+  version="0.9.10.1",
   license="GNU GPLv2",
   keywords="kafka kafka-crypto kafka-security security crypo",
   author="Tyrel M. McQueen",
@@ -16,7 +16,7 @@ setuptools.setup(
   url="https://github.com/tmcqueen-materials/kafkacrypto",
   packages=setuptools.find_packages(),
   python_requires='>=3.3',
-  install_requires=['pysodium>=0.7.5','msgpack>=1.0.0','kafka-python>=1.4.4','certifi','dirhash>=0.2.1'],
+  install_requires=['pysodium>=0.7.5','msgpack>=1.0.0','certifi','dirhash>=0.2.1','kafka-python @ git+https://github.com/dpkp/kafka-python@0864817 ; python_version>="3.12"','kafka-python>=1.4.4; python_version<"3.12"'],
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
