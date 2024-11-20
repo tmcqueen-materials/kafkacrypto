12945ac5db2fe78414f110f5e8ea5b66257c742e
Tyrel M. McQueen <tmcqueen@github.com>
2024-11-20 08:27:43 -0500

    Prepare for 0.9.11.0a0 release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="kafkacrypto",
-  version="0.9.10.4dev0",
+  version="0.9.11.0a0",
   license="GNU GPLv2",
   keywords="kafka kafka-crypto kafka-security security crypo",
   author="Tyrel M. McQueen",
